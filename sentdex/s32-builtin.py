11b3ae6f34e4618bce10016b3a222e9cb920868f
blitz <blitz70@hanmail.net>
2017-02-14 05:16:50 +0900

    python tutorial update

---
+++ sentdex/s32-builtin.py
@@ -0,0 +1,35 @@
+#   32. Built-in Functions
+
+#help()
+
+print('abs--------')
+n1 = -5
+n2 = 5
+
+print(abs(n1))
+
+if abs(n1)==abs(n2):
+    print('same number')
+
+print('min max--------')
+l = [5,2,6,3,8,7]
+print(max(l))
+print(min(l))
+
+print('round--------')
+n3 = 5.627
+print(round(n3))
+print(round(n3,1))
+print(round(n3,2))
+
+print('int str--------')
+intMe = '55'
+print(intMe)
+print(int(intMe))
+print(float(intMe))
+strMe = 77
+print(str(strMe))
+
+
+
+
---
+++ sentdex/s33-os.py
@@ -0,0 +1,18 @@
+#   33. OS
+
+import os, time
+
+
+#os.system('ipconfig')
+#os.makedirs('d:\\pythontest/dir1/dir2/dir3')
+
+curDir = os.getcwd()
+print(curDir)
+print(os.listdir())
+os.mkdir('d:\\pythontest')
+time.sleep(5)
+os.rename('d:/pythontest','d:/PYTHONTEST2')
+time.sleep(5)
+os.rmdir('d:/PYTHONTEST2')
+
+
