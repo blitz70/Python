bcb5887e3c353bfdaf347563f6836ae953c73835
blitz <blitz70@hanmail.net>
2017-02-19 03:22:32 +0900

    Python update, directory

--- sentdex1/s35_urllib.py
+++ sentdex1/s35_urllib.py
@@ -28,7 +28,7 @@ except Exception as e:
     print(str(e))
 
 try:
-    url = 'https://www.google.com/search?q=sentdex'
+    url = 'https://www.google.com/search?q=sentdex1'
     myheaders = {}
     myheaders['User-Agent'] = 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17'
     req = urllib.request.Request(url, headers=myheaders)
