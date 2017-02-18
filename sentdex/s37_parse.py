1f1ac0e8b6063c6ccd6cb4c0c58fe28c24e13e73
blitz <blitz70@hanmail.net>
2017-02-18 20:04:10 +0900

    python update

--- sentdex/s37_parse.py
+++ sentdex/s37_parse.py
@@ -3,12 +3,17 @@
 import re
 import urllib.request as rq, urllib.parse as ps
 
+
 url = 'https://pythonprogramming.net/parse-website-using-regular-expressions-urllib/'
 req = rq.Request(url)
 resp = rq.urlopen(req)
 respData = resp.read()
 #print(respData)
 
-paragraphs = re.findall('<p>(.*)</p>',str(respData))
+paragraphs = re.findall('<p>(.*?)</p>',str(respData))
 for paragraph in paragraphs:
     print(paragraph)
+
+s = '<html><head><title>Title</title>'
+result = re.match('<.*?>', s)
+print(result.group())
