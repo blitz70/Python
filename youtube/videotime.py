5f8812940d9b11217ae02a89821a21a43fce0187
blitz <blitz70@hanmail.net>
2017-02-28 07:51:22 +0900

    youtube timer update

--- youtube/videotime.py
+++ youtube/videotime.py
@@ -4,30 +4,29 @@ import csv
 
 with open('geof.txt') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=':')
-    day = 0
-    hr = 0
-    min = 0
-    sec = 0
+    days = 0
+    hrs = 0
+    mins = 0
+    secs = 0
     for t in readCSV:
         if not t:
-           pass
+           pass #skip blank line
         else:
-            #print(t)
             if len(t)>2:
-                hr += int(t[0])
-                min += int(t[1])
-                sec += int(t[2])
+                hrs += int(t[0])
+                mins += int(t[1])
+                secs += int(t[2])
             else:
-                min += int(t[0])
-                sec += int(t[1])
-            if hr >= 24:
-                day += 1
-                hr -= 24
-            if min >= 60:
-                hr += 1
-                min -= 60
-            if sec >= 60:
-                min += 1
-                sec -= 60
-        #print(t, day, hr, min, sec)
-    print('Playlist duration',str(day)+'d',str(hr)+'h',str(min)+'m',str(sec)+'s')
+                mins += int(t[0])
+                secs += int(t[1])
+            if hrs >= 24:
+                days += 1
+                hrs -= 24
+            if mins >= 60:
+                hrs += 1
+                mins -= 60
+            if secs >= 60:
+                mins += 1
+                secs -= 60
+        #print(t, days, hrs, mins, secs)
+    print('Playlist duration',str(days)+'d',str(hrs)+'h',str(mins)+'m',str(secs)+'s')
