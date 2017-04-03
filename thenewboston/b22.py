fdd1daa5b29687c8a6250aa768cc1b207019989d
blitz <blitz70@hanmail.net>
2017-04-04 04:22:17 +0900

    Python update, thenewboston

---
+++ thenewboston/b22.py
@@ -0,0 +1,9 @@
+import random as rd
+import urllib.request as rq
+
+def download_web_image(url):
+    no = rd.randrange(1,1000)
+    file_name = str(no) +'.jpg'
+    rq.urlretrieve(url, file_name)
+
+download_web_image('https://thenewboston.com/photos/users/2/resized/869b40793dc9aa91a438b1eb6ceeaa96.jpg')
---
+++ thenewboston/b24.py
@@ -0,0 +1,16 @@
+import urllib.request as rq
+
+goog_url = 'http://chart.finance.yahoo.com/table.csv?s=GOOG&a=2&b=4&c=2017&d=3&e=4&f=2017&g=d&ignore=.csv'
+
+def download_stock_data(csv_url):
+    resp = rq.urlopen(csv_url)
+    csv = resp.read()
+    lines = str(csv).split(r'\n')
+    print(len(lines))
+    print(lines)
+    fw = open(r'goog.csv','w')
+    for line in lines:
+        fw.write(line+'\n')
+    fw.close()
+
+download_stock_data(goog_url)
