3b92c98e3d6e78aa93b4e1210a5e3ca5b11ac816
blitz <blitz70@hanmail.net>
2017-04-28 02:18:59 +0900

    Flask : bootstrap, templates code/note update : sentdex

--- Flask/sentdex/__init__.py
+++ Flask/sentdex/__init__.py
@@ -7,6 +7,9 @@ app.config.update(TEMPLATES_AUTO_RELOAD = True)
 def homepage():
     return render_template('main.html')
 
+@app.route('/dashboard/')
+def dashboard():
+    return render_template('dashboard.html')
 
 if __name__ == "__main__":
     app.run()
