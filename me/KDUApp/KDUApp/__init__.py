23209c0c24cdb7605fbea9f40f0b398819a031de
blitz <blitz70@hanmail.net>
2017-05-11 08:12:22 +0900

    flask-kdu update 	data structure, save & load 	result page

--- me/KDUApp/KDUApp/__init__.py
+++ me/KDUApp/KDUApp/__init__.py
@@ -1,48 +1,64 @@
 from flask import Flask, render_template, request, url_for, redirect
 import gc
-
+import json
 
 app = Flask(__name__)
 app.config.update(TEMPLATES_AUTO_RELOAD=True)
 
-
 root_path = ''
+
 
 @app.route('/')
 def homepage():
     return render_template('home.html', root_path=root_path)
 
+
 @app.route('/survey_industry/')
 def survey_industry():
     return render_template('survey_industry.html', root_path=root_path)
 
+
 @app.route('/survey_start/', methods=['GET', 'POST'])
 def survey_start():
     if request.method == 'POST':
-        results_l = []
+        results = []
         for item in request.form:
             if item == 'submit':
                 continue
-            results_l.append([item, request.form[item]])
-        save_data(results_l)
-        # return render_template('survey_done.html', results=results_l, root_path=root_path)
+            results.append([item, request.form[item]])
+        results.sort(key=lambda x: x[0])
+        save_data(results, '/var/www/KDUApp/KDUApp/data/results.txt')
+        # return render_template('survey_results.html', results=results_l, root_path=root_path)
         return redirect("http://inhappy.kr/mc/?id=KDUBMLS")
     return render_template('survey_start.html', root_path=root_path)
 
 
-def save_data(data):
-    data_str = str(str(data).encode('utf-8'))
+@app.route('/survey_results/')
+def survey_results():
+    results = load_data('/var/www/KDUApp/KDUApp/data/results.txt')
+    return render_template('survey_results.html', results=results, root_path=root_path)
+
+
+def save_data(data, file):
     try:
-        with open('/var/www/KDUApp/KDUApp/data/results.txt', 'a') as f:
-            f.write(data_str + '\n')
-    except:
-        pass
+        with open(file, 'a') as f:
+            json_txt = json.dumps(data)
+            f.write(json_txt + '\n')
+    except Exception as e:
+        print(e)
     gc.collect()
 
 
-@app.route('/survey_end/')
-def survey_done():
-    return render_template('survey_done.html', results=results_l, root_path=root_path)
+def load_data(file):
+    try:
+        data = []
+        with open(file, 'r') as f:
+            for line in f.readlines():
+                data.append(json.loads(line))
+        return data
+    except Exception as e:
+        print(e)
+    gc.collect()
 
 if __name__ == "__main__":
     app.run()
