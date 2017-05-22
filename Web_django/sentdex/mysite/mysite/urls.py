54fe1046dd78be35602334eb7ed3f6b07292fa32
blitz <blitz70@hanmail.net>
2017-05-22 12:39:59 +0900

    Django: templates static bootstrap sentdex

--- Web_django/sentdex/mysite/mysite/urls.py
+++ Web_django/sentdex/mysite/mysite/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^webapp/', include('webapp.urls')),
+    url(r'^$', include('personal.urls')),
 ]
