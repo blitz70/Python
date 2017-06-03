34cec3495ca6f57ed16186ceec820cb708188484
blitz <blitz70@hanmail.net>
2017-06-03 19:47:12 +0900

    Django: bootstrap, add album thenewboston

--- Web_django/thenewboston/mysite/music/urls.py
+++ Web_django/thenewboston/mysite/music/urls.py
@@ -6,6 +6,7 @@ app_name = "music"
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name="index"),  # /
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name="detail"),  # /<pk>/
+    url(r'^album/add/$', views.add_album, name="add_album")
 ]
 
 '''urlpatterns = [
--- Web_django/thenewboston/mysite/music/views.py
+++ Web_django/thenewboston/mysite/music/views.py
@@ -1,5 +1,7 @@
 from django.views import generic
 from .models import Album
+from django.shortcuts import render
+from datetime import date as d
 
 
 class IndexView(generic.ListView):
@@ -14,3 +16,15 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     template_name = "music/detail.html"
     model = Album
+
+
+def add_album(request):
+    if request.method == "POST":
+        artist = request.POST["artist"]
+        title = request.POST["title"]
+        genre = request.POST["genre"]
+        logo = request.POST["logo"]
+        album = Album(artist=artist, title=title, genre=genre, logo=logo, date=d.today())
+        album.save()
+        return render(request, "music/detail.html", {"album": album})
+    return render(request, "music/add_album.html")
