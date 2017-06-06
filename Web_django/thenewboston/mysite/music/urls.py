c8277bb5b086ceefaf3c95877f91e5bb9a15ab96
blitz <blitz70@hanmail.net>
2017-06-07 03:06:29 +0900

    Django: user registration thenewboston

--- Web_django/thenewboston/mysite/music/urls.py
+++ Web_django/thenewboston/mysite/music/urls.py
@@ -5,6 +5,7 @@ app_name = "music"
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name="index"),
+    url(r'^register/$', views.UserFormView.as_view(), name="register"),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name="detail"),
     url(r'^album/add/$', views.AlbumCreateView.as_view(), name="add_album"),
     url(r'^album/(?P<pk>\d+)/$', views.AlbumUpdateView.as_view(), name="update_album"),
--- Web_django/thenewboston/mysite/music/views.py
+++ Web_django/thenewboston/mysite/music/views.py
@@ -1,7 +1,10 @@
 from django.views import generic
 from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
-from .models import Album
+from music.models import Album
+from music.forms import UserForm
 
 
 class IndexView(generic.ListView):
@@ -31,3 +34,27 @@ class AlbumUpdateView(generic.edit.UpdateView):
 class AlbumDeleteView(generic.edit.DeleteView):
     model = Album
     success_url = reverse_lazy("music:index")
+
+
+class UserFormView(generic.View):
+    form_class = UserForm
+    template_name = "music/registration_form.html"
+
+    def get(self, request):  # before submit
+        form = self.form_class()
+        return render(request, self.template_name, {"form": form})
+
+    def post(self, request):  # after submit
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user.set_password(password)
+            user.save()
+            user = authenticate(username=username, password=password)
+            if user:
+                if user.is_active:
+                    login(request, user)
+                    return redirect("music:index")
+        return redirect(request, self.template_name, {"form": form})
