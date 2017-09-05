f2312bcadc8c57160fe2fba8a3404f8ae0d45e05
blitz <blitz70@hanmail.net>
2017-09-05 20:15:40 +0900

    Intermediate: OOP, inheritance

---
+++ Core/intermediate/blob17.py
@@ -0,0 +1,27 @@
+import random
+
+
+class Blob:
+
+    def __init__(self, color, x_boundary, y_boundary, size_range=(4, 8), movement_range=(-1, 2)):
+        self.color = color
+        self.x_boundary = x_boundary
+        self.y_boundary = y_boundary
+        self.x = random.randrange(0, self.x_boundary)
+        self.y = random.randrange(0, self.y_boundary)
+        self.size = random.randrange(*size_range)
+        self.movement = movement_range
+
+    def move(self):
+        self.x += random.randrange(*self.movement)
+        self.y += random.randrange(*self.movement)
+
+    def check_bounds(self):
+        if self.x < 0:
+            self.x = 0
+        elif self.x > self.x_boundary:
+            self.x = self.x_boundary
+        if self.y < 0:
+            self.y = 0
+        elif self.y > self.y_boundary:
+            self.y = self.y_boundary
--- Core/intermediate/s17_oop_inheritance.py
+++ Core/intermediate/s17_oop_inheritance.py
@@ -1,15 +1,14 @@
 import pygame
-from Core.intermediate.blob16 import Blob
+import random
+from Core.intermediate.blob17 import Blob
 
-STARTING_RED_BLOBS = 13
-STARTING_GREEN_BLOBS = 50
-STARTING_BLUE_BLOBS = 100
+STARTING_RED_BLOBS = 3
+STARTING_BLUE_BLOBS = 10
 
 WIDTH = 800
 HEIGHT = 600
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
-GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
 game_display = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -17,21 +16,29 @@ pygame.display.set_caption("Blob World")
 clock = pygame.time.Clock()
 
 
+class BlueBlob(Blob):
+    def __init__(self):
+        super().__init__(BLUE, WIDTH, HEIGHT)
+
+    def move_fast(self):
+        self.x += random.randrange(*self.movement)*7
+        self.y += random.randrange(*self.movement)*7
+
+
 def draw_environment(blobs_dict):
     game_display.fill(WHITE)
     for blob_id in blobs_dict:
         blob = blobs_dict[blob_id]
         pygame.draw.circle(game_display, blob.color, (blob.x, blob.y), blob.size)
-        blob.move()
+        blob.move_fast()
         # blob.check_bounds()
     pygame.display.update()
 
 
 def main():
-    red_blobs = [Blob(RED, WIDTH, HEIGHT, size_range=(20, 41), movement_range=(-2, 3)) for _ in range(STARTING_RED_BLOBS)]
-    green_blobs = [Blob(GREEN, WIDTH, HEIGHT) for _ in range(STARTING_GREEN_BLOBS)]
-    blue_blobs = [Blob(BLUE, WIDTH, HEIGHT, size_range=(2, 5), movement_range=(-8, 9)) for _ in range(STARTING_BLUE_BLOBS)]
-    blobs = dict(enumerate(blue_blobs + green_blobs + red_blobs))
+    red_blobs = [BlueBlob() for _ in range(STARTING_RED_BLOBS)]
+    blue_blobs = [BlueBlob() for _ in range(STARTING_BLUE_BLOBS)]
+    blobs = dict(enumerate(blue_blobs + red_blobs))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
